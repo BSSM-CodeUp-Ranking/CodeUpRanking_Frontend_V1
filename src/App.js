0e4bf9f3021874aec6f031e25d7708cb1eae95b2
seonghoon07 <kkukkuli07@gmail.com>
2023-08-21 00:15:38 +0900

    ADD :: background-color

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+import {styled , createGlobalStyle} from 'styled-components'
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <Main>
+      <GlobalStyle />
+    </Main>
   );
 }
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+  }
+`
+
+const Main = styled.div`
+  background-color: #202124;
+  width: 100%;
+  height: 100vh;
+
+`;
 
 export default App;
